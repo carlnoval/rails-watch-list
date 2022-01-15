28a5c063561b9cabd6a3cf856588b2b7d8997473
Carl Noval <cjrnoval22@gmail.com>
2022-01-15 14:39:38 +0900

    bare min lists controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # List
+  # - lists-
+  # A user can see all the lists                              GET "lists"
+  # A user can see the details of a given list and its name   GET "lists/42"
+  # A user can create a new list                              GET "lists/new"
+  #                                                           POST "lists"
 
-  # A user can see all the lists
-  # GET "lists"
-  # A user can see the details of a given list and its name
-  # GET "lists/42"
-  # A user can create a new list
-  # GET "lists/new"
-  # POST "lists"
+  # this command also creates the erb files 
+  # rails g controller lists index show new creates: empty method actions, erb files
   resources :lists, only: [ :index, :show, :new, :create ]
 end
